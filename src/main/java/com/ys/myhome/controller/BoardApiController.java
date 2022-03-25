74378833f2f31a4f322e7b51a2c1af227d4b8c25
ys <kangyoonsoon@gmail.com>
2022-03-25 14:04:35 -0400

    Implement of REST API for Board

--- src/main/java/com/ys/myhome/controller/BoardApiController.java
+++ src/main/java/com/ys/myhome/controller/BoardApiController.java
@@ -1,4 +1,66 @@
 package com.ys.myhome.controller;
 
-public class BoardApiController {
+
+import com.ys.myhome.model.Board;
+import com.ys.myhome.repository.BoardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.thymeleaf.util.StringUtils;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+class BoardApiController {
+
+    @Autowired
+    private BoardRepository boardRepository;
+
+
+    // Aggregate root
+    // tag::get-aggregate-root[]
+    @GetMapping("/boards")
+    List<Board> all(@RequestParam(required = false, defaultValue = "") String title,
+                    @RequestParam(required = false, defaultValue = "") String content
+    ) {
+
+        if (StringUtils.isEmpty(title) && StringUtils.isEmpty(content)) {
+           return boardRepository.findAll();
+        } else {
+            return boardRepository.findByTitleOrContent(title, content);
+        }
+    }
+    // end::get-aggregate-root[]
+
+    @PostMapping("/boards")
+    Board newBoard(@RequestBody Board newBoard) {
+        return boardRepository.save(newBoard);
+    }
+
+    // Single item
+
+    @GetMapping("/boards/{id}")
+    Board one(@PathVariable Long id) {
+        return boardRepository.findById(id).orElse(null);
+    }
+
+    @PutMapping("/boards/{id}")
+    Board replaceBoard(@RequestBody Board newBoard, @PathVariable Long id) {
+
+        return boardRepository.findById(id)
+                .map(board -> {
+                    board.setTitle(newBoard.getTitle());
+                    board.setContent(newBoard.getContent());
+                    return boardRepository.save(board);
+                })
+                .orElseGet(() -> {
+                    newBoard.setId(id);
+                    return boardRepository.save(newBoard);
+                });
+    }
+
+    @DeleteMapping("/boards/{id}")
+    void deleteBoard(@PathVariable Long id) {
+        boardRepository.deleteById(id);
+    }
 }
